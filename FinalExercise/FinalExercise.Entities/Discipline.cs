598f1edc14f1a23ab739026b479232a8e224687d
Anatoly Konoplev <anatoly.konoplev@monopoly.su>
2026-07-07 19:58:31 +0300

    Подготовил сущности

---
+++ FinalExercise/FinalExercise.Entities/Discipline.cs
@@ -0,0 +1,19 @@
+﻿using FinalExercise.Dal.Contracts;
+
+namespace FinalExercise.Entities;
+
+/// <summary>
+/// Предмет
+/// </summary>
+public class Discipline : BaseAuditEntity
+{
+    /// <summary>
+    /// Наименование
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Описание
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+}
---
+++ FinalExercise/FinalExercise.Entities/Person.cs
@@ -0,0 +1,34 @@
+﻿using FinalExercise.Dal.Contracts;
+
+namespace FinalExercise.Entities;
+
+/// <summary>
+/// Сущность участника
+/// </summary>
+public class Person : BaseAuditEntity
+{
+    /// <summary>
+    /// Фамилия
+    /// </summary>
+    public string LastName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Имя
+    /// </summary>
+    public string FirstName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Отчество
+    /// </summary>
+    public string Patronymic { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Адрес электронной почты
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Телефон
+    /// </summary>
+    public string Phone { get; set; } = string.Empty;
+}
